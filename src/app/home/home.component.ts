ce78c2dee6cc94b7018256f222f4204f5669907c
arish11 <abhishekarya.8596@gmail.com>
2023-07-07 18:51:16 +0530

    accepting dynamic routes from url

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route : Router) { }
 
   ngOnInit() {
+  }
+
+  toServers(){
+    this.route.navigate(['/servers'])
+  }
+
+  toUsers(){
+    this.route.navigate(['/users'])
   }
 
 }
