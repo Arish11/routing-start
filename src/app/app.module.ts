cc0025819c0b4384cf2ac84c8d5c1246d4ab2c7f
arish11 <abhishekarya.8596@gmail.com>
2023-07-09 01:48:15 +0530

    adding a new not-found component for wildcard routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes : Routes = [
   {path:'',component:HomeComponent},
@@ -22,7 +23,9 @@ const appRoutes : Routes = [
     {path:":id",component:ServerComponent},
     {path:":id/edit",component:EditServerComponent},
   ]},
- 
+  {path:"not-found", component:NotFoundComponent},
+  {path:"**", redirectTo:"not-found"}
+
 ]
 
 @NgModule({
@@ -33,7 +36,8 @@ const appRoutes : Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
